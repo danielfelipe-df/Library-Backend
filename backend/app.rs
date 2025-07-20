c0ac425a8e7dd3e48c2fc6ac68e4300b7a70ba66
Daniel Felipe <danielfoc@protonmail.com>
2025-07-19 19:05:35 -0500

    feat(Back): Configure CORS for request from browser

--- backend/app.rs
+++ backend/app.rs
@@ -1,5 +1,6 @@
 
 use actix_web::{get, App, HttpServer,  HttpResponse, Responder};
+use actix_cors::Cors;
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -8,7 +9,7 @@ async fn index() -> impl Responder {
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-	HttpServer::new(|| App::new().service(index))
+	HttpServer::new(|| App::new().wrap(Cors::permissive()).service(index))
 		.bind("127.0.0.1:9802")?
 		.run()
 		.await
