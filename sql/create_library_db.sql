498b8b5db6e7f52048971af12d71ae3c0a08b7d5
Daniel Felipe <danielfoc@protonmail.com>
2025-08-02 17:18:57 -0500

    feat(SQL): Add SQL script to create the Database library

---
+++ sql/create_library_db.sql
@@ -0,0 +1,11 @@
+-- Create the database 'library' with 'daniel' user as owner.
+-- The variables ENCODING, LC_COLLATE and LC_CTYPE  with the values
+-- shown let support Spanish characters.
+-- The line 'TEMPLATE template0' let create the DB without any template behind
+
+CREATE DATABASE library
+	OWNER daniel
+	ENCODING 'UTF8'
+	LC_COLLATE 'es_ES.UTF-8'
+	LC_CTYPE 'es_ES.UTF-8'
+	TEMPLATE template0;
